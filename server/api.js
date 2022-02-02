0375363100101572b07a4506c9d9934a48a7e8f9
Azakkiel <azakkiel@gmail.com>
2022-02-02 10:19:34 +0100

    delete minions,ideas by id and all meetings

--- server/api.js
+++ server/api.js
@@ -12,7 +12,10 @@ apiRouter
 .route('/minions/:minionId')
 .get((req,res) => {res.send(db.getFromDatabaseById('minions',req.params.minionId));})
 .put((req,res) => {})
-.delete((req,res) => {});
+.delete((req,res) => {
+    db.deleteFromDatabasebyId('minions', req.params.minionId);
+    res.sendStatus(200);
+});
 
 //ideas routes
 apiRouter
@@ -24,13 +27,19 @@ apiRouter
 .route('/ideas/:ideaId')
 .get((req,res) => {res.send(db.getFromDatabaseById('ideas',req.params.ideaId));})
 .put((req,res) => {})
-.delete((req,res) => {});
+.delete((req,res) => {
+    db.deleteFromDatabasebyId('ideas', req.params.ideaId);
+    res.sendStatus(200);
+});
 
 //meetings routes
 apiRouter
 .route('/meetings')
 .get((req,res) => {res.send(db.getAllFromDatabase('meetings'));})//get all meetings
 .post((req,res) => {})
-.delete((req,res) => {});
+.delete((req,res) => {
+    db.deleteAllFromDatabase('meetings');
+    res.sendStatus(200);
+});
 
 module.exports = apiRouter;
