ce33411ce81f6f2b27da80d2b233bf582091a1f6
Azakkiel <azakkiel@gmail.com>
2022-02-02 10:10:15 +0100

    get ideas/minions by id

--- server/api.js
+++ server/api.js
@@ -5,37 +5,31 @@ const getAllFromDatabase =
 //minions routes
 apiRouter
 .route('/minions')
-.get((req,res) => {
-    res.send(db.getAllFromDatabase('minions'));
-})
+.get((req,res) => {res.send(db.getAllFromDatabase('minions'));})//get all minions
 .post((req,res) =>{});
 
 apiRouter
 .route('/minions/:minionId')
-.get((req,res) => {})
+.get((req,res) => {res.send(db.getFromDatabaseById('minions',req.params.minionId));})
 .put((req,res) => {})
 .delete((req,res) => {});
 
 //ideas routes
 apiRouter
 .route('/ideas')
-.get((req,res) => {
-    res.send(db.getAllFromDatabase('ideas'));
-})
+.get((req,res) => {res.send(db.getAllFromDatabase('ideas'));})//get all ideas
 .post((req,res) =>{});
 
 apiRouter
 .route('/ideas/:ideaId')
-.get((req,res) => {})
+.get((req,res) => {res.send(db.getFromDatabaseById('ideas',req.params.ideaId));})
 .put((req,res) => {})
 .delete((req,res) => {});
 
 //meetings routes
 apiRouter
 .route('/meetings')
-.get((req,res) => {
-    res.send(db.getAllFromDatabase('meetings'));
-})
+.get((req,res) => {res.send(db.getAllFromDatabase('meetings'));})//get all meetings
 .post((req,res) => {})
 .delete((req,res) => {});
 
