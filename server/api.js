d5c2255b3aa4e63eb4e779abe3c09b903e60dc1c
Azakkiel <azakkiel@gmail.com>
2022-02-06 10:09:48 +0100

    api router done

--- server/api.js
+++ server/api.js
@@ -28,8 +28,8 @@ apiRouter
 .route('/minions')
 .get((req,res) => {res.send(db.getAllFromDatabase('minions'));})//get all minions
 .post(bodyParser.json(),(req,res) =>{
-    db.addToDatabase('minions', req.body);
-    res.sendStatus(201);
+    const newMinion = db.addToDatabase('minions', req.body);
+    res.status(201).send(newMinion);
 });
 
 apiRouter
@@ -51,8 +51,8 @@ apiRouter
 .route('/ideas')
 .get((req,res) => {res.send(db.getAllFromDatabase('ideas'));})//get all ideas
 .post(bodyParser.json(),(req,res) =>{
-    db.addToDatabase('ideas', req.body);
-    res.sendStatus(201);
+    const newIdea = db.addToDatabase('ideas', req.body);
+    res.status(201).send(newIdea);
 });
 
 apiRouter
@@ -60,7 +60,7 @@ apiRouter
 .get(checkIdeaId,(req,res) => {res.send(db.getFromDatabaseById('ideas',req.params.ideaId));})
 .put(bodyParser.json(), checkIdeaId ,(req,res) => {
     db.updateInstanceInDatabase('ideas', req.body);
-    res.sendStatus(200);
+    res.status(200).send(db.getFromDatabaseById('ideas',req.params.ideaId));
 })
 .delete(checkIdeaId, (req,res) => {
     db.deleteFromDatabasebyId('ideas', req.params.ideaId);
@@ -72,8 +72,7 @@ apiRouter
 .route('/meetings')
 .get((req,res) => {res.send(db.getAllFromDatabase('meetings'));})//get all meetings
 .post((req,res) => {
-    db.addToDatabase('meetings',db.createMeeting());
-    res.sendStatus(201);
+    res.status(201).send(db.addToDatabase('meetings',db.createMeeting()));
 })
 .delete((req,res) => {
     db.deleteAllFromDatabase('meetings');
@@ -81,31 +80,3 @@ apiRouter
 });
 
 module.exports = apiRouter;
-
-
-/*
-/api/minions
-
-GET /api/minions to get an array of all minions. D 
-POST /api/minions to create a new minion and save it to the database. D
-GET /api/minions/:minionId to get a single minion by id. D 
-non numeric id check, invalid id check D
-PUT /api/minions/:minionId to update a single minion by id. D
-need to return updated minion, non numeric id, invalid id D
-DELETE /api/minions/:minionId to delete a single minion by id. D
-non numeric id, invalid id
-
-/api/ideas
-
-GET /api/ideas to get an array of all ideas. D
-POST /api/ideas to create a new idea and save it to the database. D
-GET /api/ideas/:ideaId to get a single idea by id. D
-PUT /api/ideas/:ideaId to update a single idea by id. D
-DELETE /api/ideas/:ideaId to delete a single idea by id. D
-
-/api/meetings
-
-GET /api/meetings to get an array of all meetings. D
-POST /api/meetings to create a new meeting and save it to the database.D
-DELETE /api/meetings to delete all meetings from the database. D
-*/
